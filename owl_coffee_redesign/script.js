224e18be859b27cf47afdd642107e68948a8a987
Chewie <rcchew2004@gmail.com>
2024-02-11 20:35:21 +0000

    ~ to pc

--- owl_coffee_redesign/script.js
+++ owl_coffee_redesign/script.js
@@ -6,3 +6,27 @@ function showMenu() {
 function hideMenu() {
     navMenu.style.right = "-160px";
 }
+
+// Scroll Animation
+
+const scrollerRows = document.querySelectorAll('.scroller');
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimationRow();
+}
+
+
+function addAnimationRow() {
+    scrollerRows.forEach(scrollerRow => { // Scroller row
+        scrollerRow.setAttribute('data-animated', true);
+
+        const scrollerInner = document.querySelector('.scroller-items'); // row
+        const scrollerContent = Array.from(scrollerInner.children); // duplicate child wrap?
+
+        scrollerContent.forEach(item => { // duplicate children/items
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden', true);
+            scrollerInner.appendChild(duplicatedItem);
+        })
+    })
+}
